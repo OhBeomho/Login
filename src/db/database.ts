fd83b9f83607139f6ded4f84265b46d11fca1af7
OhBeomho <tyler0322@naver.com>
2022-12-31 12:49:55 +0900

    d

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 
 const sqlite = sqlite3.verbose();
-export const db = new sqlite.Database("db/accdb.db", sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE, (err) => {
+export const db = new sqlite.Database("db/database.db", sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE, (err) => {
 	if (err) {
 		console.log(err.message);
 		process.exit(1);
